26986a1daddef5326e28efb4e04fa01999e22821
ghiboz <info@ghiboz.com>
2019-03-21 14:58:53 +0100

    hide object and telone :+1:

---
+++ Objects/HideObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour
+{
+    public List<HideVisible> hideVisible;
+}
+
+[System.Serializable]
+public class HideVisible
+{
+    public float beginVisible;
+    public float endVisible;
+}
