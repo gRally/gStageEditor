e8d7121c9fe21c5b6db5e1241eb83c6c3729f07d
ghiboz <info@ghiboz.com>
2019-05-02 10:36:33 +0200

    Enable Gravity script https://i.imgur.com/8SR0qB9.png

---
+++ Runtime/Objects/EnableGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGravity : MonoBehaviour
+{
+    Rigidbody rb;
+    Collider col;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        rb.useGravity = false;
+        col.isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            col.isTrigger = false;
+            rb.useGravity = true;
+        }
+    }
+}
